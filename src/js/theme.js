779f7aec31f646807be7e94832a3568cf84858f7
Shane <6071159+smashedr@users.noreply.github.com>
2026-02-23 17:56:39 -0800

    Lint

--- src/js/theme.js
+++ src/js/theme.js
@@ -74,7 +74,8 @@
 
         document.querySelectorAll('[data-bs-theme-value]').forEach((el) => {
             el.addEventListener('click', () => {
-                const value = el.getAttribute('data-bs-theme-value')
+                // const value = el.getAttribute('data-bs-theme-value')
+                const value = el.dataset.bsThemeValue
                 setStoredTheme(value)
                 setTheme(value)
                 showActiveTheme(value)
