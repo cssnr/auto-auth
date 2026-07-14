551b1ca9749b7eff37390a0d11aa95c59fc6681e
Shane <6071159+smashedr@users.noreply.github.com>
2026-07-14 09:19:20 -0700

    Lint

--- src/entrypoints/background/auth.ts
+++ src/entrypoints/background/auth.ts
@@ -16,8 +16,7 @@ export function onAuthRequired(
 
 export function webRequestFinished(
   requestDetails:
-    | chrome.webRequest.OnCompletedDetails
-    | chrome.webRequest.OnErrorOccurredDetails,
+    chrome.webRequest.OnCompletedDetails | chrome.webRequest.OnErrorOccurredDetails,
 ): void {
   const index = pendingRequests.indexOf(requestDetails.requestId)
   if (index > -1) {
